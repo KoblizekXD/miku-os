86f91d98ac9956eb5e02b0cde7d437d86e7e3245
KoblizekXD <janprokupek@centrum.cz>
2024-11-20 13:52:55 +0100

    Move memory functions into a separate file

--- src/util/memio.h
+++ src/util/memio.h
@@ -3,6 +3,10 @@
 
 #include <stddef.h>
 
+void* memcpy(void* dest, const void* src, size_t count);
+void *memset(void *address, int c, size_t count);
+void *memmove(void *dest, const void *src, size_t count);
+int memcmp(const void *s1, const void *s2, size_t count);
 size_t strlen(const char* start);
 
 #endif
